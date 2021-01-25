d601b6fdf24d17e9b792fc20b675bb43be1c0ff7
Kyle Thousand <KAthousand@gmail.com>
2021-01-25 16:22:42 -0500

    authentication functionality set up for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decode(token)
+    decoded = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new decoded
+  end
+
+  def authorize_request
+    header = request.headers['Authorization'] # grab the header
+    token = header.split(' ').last if header # split the token off from the header
+    begin
+      @decoded = decode(token) # decode the token from the header.
+      @current_user = User.find(@decoded[:id]) # create @current_user variable by running find with the ID from the decoded token.
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
